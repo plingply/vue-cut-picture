6e5855b91eccff946a1917f94679864fe3f59c5f
penglin <penglin@penglin.local>
2018-11-20 17:33:05 +0800

    x

--- src/main.js
+++ src/main.js
@@ -34,6 +34,10 @@ let vueCutPicture = {
             type: Boolean,
             default: true
         },
+        compress: {
+            type: Boolean,
+            default: true
+        },
         confirmText: {
             type: String,
             default: '取消'
@@ -54,12 +58,21 @@ let vueCutPicture = {
     },
     watch: {
         url(file) {
-            var fr = new FileReader();
             var self = this;
             var carbox = this.$refs.cut_carbox;
             var width = carbox.clientWidth;
+            var fr = new FileReader();
+            fr.readAsDataURL(file);
             fr.onload = function () {
-                self.imgurl = this.result;
+                if(self.compress){
+                    let img = new Image()
+                    img.src = this.result
+                    img.onload = function(){
+                        self.imgurl = self.compressImg(img)
+                    }
+                }else{
+                    self.imgurl = this.result;
+                }
                 self.$refs.caijianimg.style.maxWidth = self.width;
                 self.$refs.caijianimg.style.minWidth = width + "px";
                 self.$refs.caijianimg.style.maxHeight = self.height;
@@ -68,10 +81,60 @@ let vueCutPicture = {
                     self.setCatboxposition();
                 });
             };
-            fr.readAsDataURL(file);
         }
     },
     methods: {
+        //压缩图片
+        compressImg(img) {
+            let canvas = document.createElement("canvas");
+            let ctx = canvas.getContext('2d');
+            //瓦片canvas
+            let tCanvas = document.createElement("canvas");
+            let tctx = tCanvas.getContext("2d");
+            console.log(img)
+            let initSize = img.src.length;
+            let width = img.width;
+            let height = img.height;
+
+            console.log(width,height)
+
+            //如果图片大于四百万像素，计算压缩比并将大小压至400万以下
+            let ratio;
+            if ((ratio = width * height / 4000000) > 1) {
+                ratio = Math.sqrt(ratio);
+                width /= ratio;
+                height /= ratio;
+            } else {
+                ratio = 1;
+            }
+            canvas.width = width;
+            canvas.height = height;
+            //        铺底色
+            ctx.fillStyle = "#fff";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            //如果图片像素大于100万则使用瓦片绘制
+            let count;
+            if ((count = width * height / 1000000) > 1) {
+                count = ~~(Math.sqrt(count) + 1); //计算要分成多少块瓦片
+                //            计算每块瓦片的宽和高
+                let nw = ~~(width / count);
+                let nh = ~~(height / count);
+                tCanvas.width = nw;
+                tCanvas.height = nh;
+                for (let i = 0; i < count; i++) {
+                    for (let j = 0; j < count; j++) {
+                        tctx.drawImage(img, i * nw * ratio, j * nh * ratio, nw * ratio, nh * ratio, 0, 0, nw, nh);
+                        ctx.drawImage(tCanvas, i * nw, j * nh, nw, nh);
+                    }
+                }
+            } else {
+                ctx.drawImage(img, 0, 0, width, height);
+            }
+            //进行最小压缩
+            let ndata = canvas.toDataURL('image/jpeg', 0.1);
+            tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
+            return ndata;
+        },
         // 设置box尺寸
         setBoxsize() {
             var box = this.$refs.cut_imgbox;
