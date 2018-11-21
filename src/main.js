3ca4391111b423bf7dcd5b26f4db64bdb20581b2
penglin <penglin@penglin.local>
2018-11-21 09:38:10 +0800

    x

--- src/main.js
+++ src/main.js
@@ -90,7 +90,7 @@ export default {
 
       this.fileObj = e.srcElement;
 
-      typeof this.options.loadImgfun === "function" ? this.options.loadImgfun() : ''
+      typeof opt.loadImgfun === "function" ? opt.loadImgfun() : ''
 
       let files = e.target.files || e.dataTransfer.files;
 
